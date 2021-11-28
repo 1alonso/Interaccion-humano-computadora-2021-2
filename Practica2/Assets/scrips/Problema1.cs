d1d337e09080603c6edfc1c66040c2519ba1a07b
1alonso <jesus.mendoza10@uabc.edu.mx>
2021-11-28 12:33:19 -0800

    2
    Proyecto2

---
+++ Practica2/Assets/scrips/Problema1.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Problema1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola mundo!");
+    }
+
+    private int[] funccion2(int[] nums)
+    {
+        int[] nums = [2];
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
